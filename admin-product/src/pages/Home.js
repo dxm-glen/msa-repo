34b1d3515d30876cc4e33942373c9061d628b2b8
Kim-winmini <popshow6126@gmail.com>
2024-07-10 17:36:13 +0900

    생산공장관리자사이트

---
+++ admin-product/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h2>Welcome to the Admin Site</h2>
+            <p>Use the tabs above to navigate through the site.</p>
+        </div>
+    );
+};
+
+export default Home;
